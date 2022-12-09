1f88191db159ca49e00b45a41ebb93697f75caa0
Max Calabro <maxccalabro@gmail.com>
2022-12-09 14:15:14 -0500

    added auth to counselor routes

--- routes/CounselorRouter.js
+++ routes/CounselorRouter.js
@@ -1,17 +1,32 @@
 const router = require('express').Router()
+const middleware = require('../middleware')
 const controller = (require = require('../controllers/CounselorController'))
 
 router.get('/', controller.GetAllCounselor)
 router.get('/:counselor_id', controller.GetCounselorById)
-router.put('/:counselor_id', controller.UpdateCounselor)
+router.put(
+  '/:counselor_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateCounselor
+)
 router.put(
   '/assignStudentTo/:counselor_id',
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.AssignStudentToCounselor
 )
 router.delete(
   '/:student_id/:counselor_id',
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.RemoveStudentFromCounselor
 )
-router.delete('/:counselor_id', controller.DeleteCounselor)
+router.delete(
+  '/:counselor_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteCounselor
+)
 
 module.exports = router
