0226593f742359c47ce4a070b631b3aefe0e7b0a
Max Calabro <maxccalabro@gmail.com>
2022-12-09 11:31:36 -0500

    getAllStudents created

---
+++ controllers/StudentController.js
@@ -0,0 +1,14 @@
+const { Student } = require('../models')
+
+const GetAllStudents = async (req, res) => {
+  try {
+    const students = await Student.findAll()
+    res.send(students)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllStudents
+}
--- routes/StudentRouter.js
+++ routes/StudentRouter.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
 const controller = (require = require('../controllers/StudentController'))
 
+router.get('/', controller.GetAllStudents)
+
 module.exports = router
