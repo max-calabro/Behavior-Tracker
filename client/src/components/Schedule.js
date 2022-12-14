b10b8d887fe8a9e467e5a6c669d8d67a6d76ea4c
Max Calabro <maxccalabro@gmail.com>
2022-12-14 11:49:55 -0500

    added red/green boxes to the schedule that can be clicked

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -18,38 +18,189 @@ const Schedule = ({ studentInfo, schedule, setSchedule, selectedSchedule }) => {
     setCurrentSchedule(newS.data)
   }
 
+  const changeColor = (ofThis, color) => {
+    let toBeChanged = document.querySelector(ofThis)
+    if (color === 'green') {
+      toBeChanged.style.backgroundColor = 'rgb(8, 208, 8)'
+      //  Axios call to change behavior to positive
+    } else {
+      toBeChanged.style.backgroundColor = 'red'
+      //  Axios call to change behavior to negative
+    }
+  }
+
   useEffect(() => {
     getTheCurrentSchedule()
   }, [])
+
   return currentSchedule ? (
     <>
       <div className="student-schedule-top">
         <div>{currentSchedule.date}</div>
         <div className="between-date-and-name"></div>
-        <div>{studentInfo.name}</div>
+        <div>{currentSchedule.name}</div>
       </div>
       <div className="student-schedule-grid">
         <div className="grid-tile">
-          <div className="period-name">Period Name</div>
+          <div className="period-name">{currentSchedule.periods[0].name}</div>
+          <div className="between-name-and-style"></div>
           <div className="behavior-plan-style">
-            <div className="box-1">1</div>
-            <div className="box-2">2</div>
+            <div
+              className="box-1-0"
+              onClick={() => changeColor('.box-1-0', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-0"
+              onClick={() => changeColor('.box-2-0', 'red')}
+            >
+              2
+            </div>
           </div>
         </div>
-        <div className="grid-tile">2</div>
-        <div className="grid-tile">3</div>
-        <div className="grid-tile">4</div>
-        <div className="grid-tile">5</div>
-        <div className="grid-tile">6</div>
-        <div className="grid-tile">7</div>
+        {currentSchedule.periods.map((period, index) => (
+          <div className="grid-tile" key={period.id}>
+            <div className="period-name">{period.name}</div>
+            <div className="between-name-and-style"></div>
+            <div className="behavior-plan-style">
+              <div
+                className={'box-1-' + index}
+                onClick={() => changeColor(`.box-1-${index}`, 'green')}
+              >
+                1
+              </div>
+              <div
+                className={'box-2-' + index}
+                onClick={() => changeColor(`.box-2-${index}`, 'red')}
+              >
+                2
+              </div>
+            </div>
+          </div>
+        ))}
+        {/* <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[1].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-1"
+              onClick={() => changeColor('.box-1-1', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-1"
+              onClick={() => changeColor('.box-2-1', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[2].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-2"
+              onClick={() => changeColor('.box-1-2', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-2"
+              onClick={() => changeColor('.box-2-2', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[3].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-3"
+              onClick={() => changeColor('.box-1-3', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-3"
+              onClick={() => changeColor('.box-2-3', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[4].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-4"
+              onClick={() => changeColor('.box-1-4', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-4"
+              onClick={() => changeColor('.box-2-4', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[5].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-5"
+              onClick={() => changeColor('.box-1-5', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-5"
+              onClick={() => changeColor('.box-2-5', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
+        <div className="grid-tile">
+          <div className="period-name">{currentSchedule.periods[3].name}</div>
+          <div className="between-name-and-style"></div>
+          <div className="behavior-plan-style">
+            <div
+              className="box-1-6"
+              onClick={() => changeColor('.box-1-6', 'green')}
+            >
+              1
+            </div>
+            <div
+              className="box-2-6"
+              onClick={() => changeColor('.box-2-6', 'red')}
+            >
+              2
+            </div>
+          </div>
+        </div>
+
         <div className="grid-tile">8</div>
         <div className="grid-tile">9</div>
         <div className="grid-tile">10</div>
         <div className="grid-tile">11</div>
         <div className="grid-tile">12</div>
         <div className="grid-tile">13</div>
-        <div className="grid-tile">14</div>
-        <div className="grid-tile">15</div>
+        <div className="grid-tile">14</div> */}
+        {/* <div className="grid-tile">15</div> */}
       </div>
     </>
   ) : (
